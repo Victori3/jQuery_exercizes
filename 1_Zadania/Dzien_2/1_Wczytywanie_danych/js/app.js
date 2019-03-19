da0c1885073b3e2633b9c9d6b4d6918e947646c1
Robot CodersLab <travis-ci@coderslab.pl>
2019-03-19 09:51:37 +0100

    Initial commit

---
+++ 1_Zadania/Dzien_2/1_Wczytywanie_danych/js/app.js
@@ -0,0 +1,16 @@
+$( function() {
+
+    var movieList = $( '.list' );
+    var movieUrl = 'https://swapi.co/api/films/';
+
+    function insertContent( movies ) {
+        //tutaj zrób pętlę po filmach
+        //wygeneruj kolejne LI i wstaw do listy movieList
+    }
+
+    function loadMovies() {
+        //tutaj wykonaj połączenie Ajaxem
+    }
+
+    loadMovies();
+});
---
+++ 1_Zadania/Dzien_2/3_Wczytywanie_danych_holidays/js/app.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    var ul = $("#holidayDates");
+    var holidayUrl = '';
+
+    function instertHolidays(days) {
+        //Użyj sposobu na pobranie wartości z obiektu (nie kluczy) np. pętla for in
+    }
+
+    function loadHolidays() {
+        //tutaj wykonaj połączenie Ajaxem
+    }
+    loadHolidays();
+
+});
